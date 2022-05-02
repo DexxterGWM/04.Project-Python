9e1ad148a878450dd1b88c36f7bd6f163eee4c94
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2022-05-02 19:54:47 -0300

    File

---
+++ Script.py
@@ -0,0 +1,11 @@
+S = 'Hello world!'
+
+print(f'\n {S}') # Printing a string.
+print(f'\n {type(S)}') # Type of a string.
+print(f'\n {len(S)}') # Size of a string.
+
+print('\n My world ' + 'blue') # Concatenation.
+
+ # Replaces a substring with some other string.
+S1 = S.replace('world', 'people')
+print(f'\n {S1}')
